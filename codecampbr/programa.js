ee13f182247253f7ce263dae61e7a2c25e25fe48
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-03-10 13:46:57 -0300

    javascript

---
+++ codecampbr/programa.js
@@ -0,0 +1,52 @@
+//Tipos de dados  
+// inteiro 10
+// decimal 2.5
+// string "Pedro"
+// string 'Souza'
+/*  
+//number
+const x = 10;
+ const y = 2.5;
+
+//string
+ const nome ="Pedro";
+const sobrenome ='Souza';
+
+
+//boolean
+const verdadeiro = true;
+const falso = false;
+
+//null
+const nulo = null;
+
+//undefined
+const nao_definido = undefined;
+*/
+//console.log(typeof(x)); verificar tipo de dado
+
+//Tipos de dados 
+
+
+// Operadores aritiméticos
+const x = 4;
+const y = 5;
+
+const soma = x + y;
+const subtracao = x - y;
+const multiplicacao = x * y;
+const divisao = x / y;
+const modulo = x % y;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
+
+let z = 1;
+
+//z = z + 1; 
+//z++; aumenta 1 unidade 
+z += 6;
+console.log(z);
