b32296315417a502e92194e85eb2e914605030ed
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-03-11 01:11:18 -0300

    mais conteúdo

--- codecampbr/programa.js
+++ codecampbr/programa.js
@@ -28,7 +28,7 @@ const nao_definido = undefined;
 //Tipos de dados 
 
 
-// Operadores aritiméticos
+/* Operadores aritiméticos 
 const x = 4;
 const y = 5;
 
@@ -44,9 +44,63 @@ console.log(multiplicacao);
 console.log(divisao);
 console.log(modulo);
 
-let z = 1;
 
+// incrementação
+let z = 1;
 //z = z + 1; 
 //z++; aumenta 1 unidade 
 z += 6;
 console.log(z);
+
+
+//decrementação
+let a = 10;
+//a = a - 1;
+//a--;
+a -= 6;
+console.log(a);
+
+//multiplicação
+let c = 10;
+c*= 2;
+console.log(c);
+
+//divisão
+let d = 90;
+d /= 3;
+console.log(d);
+
+//módulo
+let e = 9;
+e %= 2;
+console.log(e);
+
+Operadores aritiméticos */
+
+
+ /* Operadores de comparação */
+ const a = 1;
+ const b = 2;
+
+ //maior ou menor// ou igual
+ console.log(a < b);
+ console.log(a > b);
+ console.log(a <= b);
+console.log(a >= b);
+
+//é igual
+console.log(a == b);
+//é diferente
+console.log(a != b);
+
+// "And"  obrigatoriamente um "e"
+console.log(a < b && a > b);
+console.log(a < b && a != b);
+console.log( a == b && a > b);
+// "Ou"
+console.log(a < b || a > b);
+console.log(a == b || a != b);
+console.log(a == b || a > b);
+
+//nao(inverte o valor)
+console.log(!(a == b));
