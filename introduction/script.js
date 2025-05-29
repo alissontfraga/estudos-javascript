513fc9f1672409525fa157710314eb3863e991f0
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-05-28 23:50:24 -0300

    New Exercise

---
+++ introduction/script.js
@@ -0,0 +1,11 @@
+//mostra no depurador console do navegador
+/* console.log(`Hello`);
+console.log(`I like pizza!`); */
+
+//aparece uma mensagem pop-up
+/* window.alert(`This is an alert!`);
+window.alert(`I like pizza!`); */
+
+//seleciona o elemento por id e aparece o seguinte:
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I lke pizza!`;
--- variables/script.js
+++ variables/script.js
@@ -1,5 +1,23 @@
-console.log(`Hello`);
-console.log(`I like pizza!`);
+//variables
 
-window.alert(`This is an alert!`);
-window.alert(`I like pizza!`);
+/* 1- declaration   let x;
+   2-  assignment    x = 100; */
+
+   /* number */
+   let age = 18;
+  let price = 10.99;
+  let gpa = 2.1;
+
+  //string   - pode incluir números
+  let firstName = "Alisso";
+
+  // boolean
+  let online = true;
+
+  console.log(`You are ${age} years old`);
+  console.log(`The price is $${price}`);
+  console.log(`Your gpa is ${gpa}`);
+  console.log(`Hello ${firstName}`);
+  console.log(`Bro is online: ${online}`);
+
+  console.log(typeof gpa); //para mostrar o tipo da variável
